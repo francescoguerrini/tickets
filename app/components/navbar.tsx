7c84584f7bbe966a1eeff4cb58db167e28498e24
Fra#33 <francescoguerrini1987@gmail.com>
2023-11-09 15:25:00 +0100

    setup prisma

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,19 +1,33 @@
+'use client'
 import React from 'react'
 import Link from 'next/link'
+import classnames from 'classnames'
+import { usePathname } from 'next/navigation'
 import { AiFillBug } from 'react-icons/ai'
 
+
 const links = [
-    {'label' : 'Dashboard', 'href' : '/' },
-    {'label' : 'Issues', 'href' : '/issues' }
+    {label : 'Dashboard', href : '/' },
+    {label : 'Issues', href : '/issues' }
 ]
 
 const Navbar = () => {
     // genera una navbar i cui link vengono mappati dall'array links definito sopra.
+    const currentPath = usePathname();
   return (
     <div className='flex h-16 items-center pl-6 px-24 border-b'>
         <Link href='/'><AiFillBug className='text-2xl hover:text-teal-500 transition-colors'/></Link>
         <ul className='flex space-x-6 pl-6'>
-            {links.map(link => <Link key={link.href} href={link.href} className='text-zinc-500 text-lg hover:text-zinc-800 transition-colors'>{link.label}</Link>)}
+            {links.map(link => <Link key={link.href}
+            href={link.href} 
+            className={classnames({
+                'text-zinc-900 font-bold': link.href === currentPath,
+                'text-zinc-500': link.href !== currentPath,
+                'hover:text-zinc-800 transition-colors': true
+            })}>
+                {link.label}
+            </Link>
+            )}
         </ul>
     </div>
   )
---
+++ app/issues/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const issues = () => {
+  return (
+    <div className='text-slate-700 text-3xl p-12'>This is the issues page</div>
+  )
+}
+
+export default issues
