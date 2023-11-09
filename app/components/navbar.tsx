30e090efdf2c0ac7698780b76b151363428596c0
Fra#33 <francescoguerrini1987@gmail.com>
2023-11-09 11:54:01 +0100

    modificata navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -10,10 +10,10 @@ const links = [
 const Navbar = () => {
     // genera una navbar i cui link vengono mappati dall'array links definito sopra.
   return (
-    <div className='flex h-16 items-center justify-between px-24 border-b'>
-        <div><AiFillBug className='text-2xl text-teal-600'/></div>
-        <ul className='flex space-x-6'>
-            {links.map(link => <Link key={link.href} href={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors'>{link.label}</Link>)}
+    <div className='flex h-16 items-center pl-6 px-24 border-b'>
+        <Link href='/'><AiFillBug className='text-2xl hover:text-teal-500 transition-colors'/></Link>
+        <ul className='flex space-x-6 pl-6'>
+            {links.map(link => <Link key={link.href} href={link.href} className='text-zinc-500 text-lg hover:text-zinc-800 transition-colors'>{link.label}</Link>)}
         </ul>
     </div>
   )
